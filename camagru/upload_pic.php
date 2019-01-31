4173ed13c2f9251df304c6c061555fbaeabf4483
Austin KHOSA <akhosa@c1r4s3.wethinkcode.co.za>
2019-01-31 14:21:46 +0200

    Hourly update

--- camagru/upload_pic.php
+++ camagru/upload_pic.php
@@ -1,14 +1,14 @@
 <?php
     ini_set('mysql.connect_timeout', 300);
-    ini_set('default_socket_timeout', 300);  
+    ini_set('default_socket_timeout', 300);
+    // Work in progress  
 ?>
-
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Upload Picture</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
@@ -21,9 +21,9 @@
         <input type="submit" name="submit" value="upload" />
     </form>
     <?php 
-        if(isset($_POST['sumit']))
+        if(isset($_POST['submit']))
         {
-            if(getimagessize($FILES['image']['tmp_name'])== FALSE)
+            if(getimagessize($_FILES['image']['tmp_name'])== FALSE)
             {
                 echo "Please select image.";
             }
@@ -33,6 +33,7 @@
                 $name= addlashes($_FILES['image']['name']);
                 $image= file_get_contents($image);
                 $image= base64_encode($image);
+                saveimage($name,$image);
             }
         }
         function saveimage()
@@ -48,7 +49,7 @@
             }
             else
             {
-                echo "<br/> Image NOT uploaded."
+                echo "<br/> Image NOT uploaded.";
             }
         }
     ?>
